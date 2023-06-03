6f49b5ee300431f6236c39c109ea3fe77a303c1f
Robin Scherrer <mail@robinscherrer.com>
2023-06-03 09:06:41 +0200

    feat(nvim)!: easier keymaps for harpoon

--- config/nvim/after/plugin/harpoon.lua
+++ config/nvim/after/plugin/harpoon.lua
@@ -1,8 +1,9 @@
 local nmap = require("rxbn.keymap").nmap
 
-nmap { '<leader>hl', '<Cmd>lua require("harpoon.ui").toggle_quick_menu()<CR>' }
-nmap { '<leader>ha', '<Cmd>lua require("harpoon.mark").add_file()<CR>' }
+nmap { '<c-e>', '<Cmd>lua require("harpoon.ui").toggle_quick_menu()<CR>' }
+nmap { '<leader>a', '<Cmd>lua require("harpoon.mark").add_file()<CR>' }
 
-for i = 1, 3 do
-  nmap { '<leader>h' .. i, '<Cmd>lua require("harpoon.ui").nav_file(' .. i .. ')<CR>' }
-end
+nmap { '<c-h>', '<Cmd>lua require("harpoon.ui").nav_file(1)<CR>' }
+nmap { '<c-j>', '<Cmd>lua require("harpoon.ui").nav_file(2)<CR>' }
+nmap { '<c-k>', '<Cmd>lua require("harpoon.ui").nav_file(3)<CR>' }
+nmap { '<c-l>', '<Cmd>lua require("harpoon.ui").nav_file(4)<CR>' }
