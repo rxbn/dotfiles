a5811ff161cc9a37be345bb4fdefe093cc1b2779
Robin Scherrer <mail@robinscherrer.com>
2023-06-12 20:17:12 +0200

    chore(nvim): use todo-comments defaults

--- config/nvim/lua/plugins/todo-comments.lua
+++ config/nvim/lua/plugins/todo-comments.lua
@@ -1,3 +1,9 @@
 return {
-  { "folke/todo-comments.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("todo-comments").setup()
+    end,
+  },
 }
