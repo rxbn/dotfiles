36fd29233d02f0334a83bdc0a267c107506ce530
Robin Scherrer <mail@robinscherrer.com>
2023-06-12 22:41:49 +0200

    chore(nvim): style fixes

--- config/nvim/lua/plugins/completion.lua
+++ config/nvim/lua/plugins/completion.lua
@@ -5,7 +5,10 @@ return {
   "hrsh7th/cmp-nvim-lua",
   "hrsh7th/cmp-nvim-lsp",
   "onsails/lspkind-nvim",
-  { "saadparwaiz1/cmp_luasnip", dependencies = { "L3MON4D3/LuaSnip" } },
+  {
+    "saadparwaiz1/cmp_luasnip",
+    dependencies = { "L3MON4D3/LuaSnip" },
+  },
   "roobert/tailwindcss-colorizer-cmp.nvim",
 
   {
--- config/nvim/lua/plugins/harpoon.lua
+++ config/nvim/lua/plugins/harpoon.lua
@@ -1,5 +1,3 @@
 return {
-  {
-    "ThePrimeagen/harpoon",
-  },
+  "ThePrimeagen/harpoon",
 }
