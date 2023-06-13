9fbfbc2ce809d76e7e5a99ab95f7484a2c394e74
Robin Scherrer <mail@robinscherrer.com>
2023-06-13 15:58:35 +0200

    refactor(nvim): harpoon

--- config/nvim/after/plugin/harpoon.lua
+++
@@ -1,9 +0,0 @@
-local nmap = require("rxbn.keymap").nmap
-
-nmap({ "<c-e>", '<Cmd>lua require("harpoon.ui").toggle_quick_menu()<CR>' })
-nmap({ "<leader>a", '<Cmd>lua require("harpoon.mark").add_file()<CR>' })
-
-nmap({ "<c-h>", '<Cmd>lua require("harpoon.ui").nav_file(1)<CR>' })
-nmap({ "<c-j>", '<Cmd>lua require("harpoon.ui").nav_file(2)<CR>' })
-nmap({ "<c-k>", '<Cmd>lua require("harpoon.ui").nav_file(3)<CR>' })
-nmap({ "<c-l>", '<Cmd>lua require("harpoon.ui").nav_file(4)<CR>' })
--- config/nvim/lua/plugins/harpoon.lua
+++ config/nvim/lua/plugins/harpoon.lua
@@ -1,3 +1,13 @@
 return {
-  "ThePrimeagen/harpoon",
+  {
+    "ThePrimeagen/harpoon",
+    keys = {
+      { "<c-e>", '<Cmd>lua require("harpoon.ui").toggle_quick_menu()<CR>' },
+      { "<leader>a", '<Cmd>lua require("harpoon.mark").add_file()<CR>' },
+      { "<c-h>", '<Cmd>lua require("harpoon.ui").nav_file(1)<CR>' },
+      { "<c-j>", '<Cmd>lua require("harpoon.ui").nav_file(2)<CR>' },
+      { "<c-k>", '<Cmd>lua require("harpoon.ui").nav_file(3)<CR>' },
+      { "<c-l>", '<Cmd>lua require("harpoon.ui").nav_file(4)<CR>' },
+    },
+  },
 }
