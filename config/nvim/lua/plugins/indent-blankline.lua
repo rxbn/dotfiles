f58308e0000c78af7e06472931a99a32fe97eff2
Robin Scherrer <mail@robinscherrer.com>
2023-06-13 15:51:57 +0200

    refactor(nvim): indent-blankline

--- config/nvim/lua/plugins/indent-blankline.lua
+++ config/nvim/lua/plugins/indent-blankline.lua
@@ -1,3 +1,13 @@
 return {
-  "lukas-reineke/indent-blankline.nvim",
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    event = {
+      "BufReadPost",
+      "BufNewFile",
+    },
+    opts = {
+      show_current_context = true,
+      show_current_context_start = true,
+    },
+  },
 }
