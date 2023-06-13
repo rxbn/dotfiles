27435166afeb13c948f76693f77fbede8be5f740
Robin Scherrer <mail@robinscherrer.com>
2023-06-13 18:30:01 +0200

    refactor(nvim): plenary

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -2,7 +2,6 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     dependencies = {
-      "nvim-lua/plenary.nvim",
       {
         "nvim-telescope/telescope-fzf-native.nvim",
         build = "make",
