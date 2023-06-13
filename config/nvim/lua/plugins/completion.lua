2f9857c3e0bb2ca1561983a57d99cb382321b58b
Robin Scherrer <mail@robinscherrer.com>
2023-06-13 19:05:28 +0200

    refactor(nvim): completion

--- config/nvim/lua/plugins/completion.lua
+++ config/nvim/lua/plugins/completion.lua
@@ -1,16 +1,96 @@
 return {
-  "hrsh7th/nvim-cmp",
-  "hrsh7th/cmp-buffer",
-  "hrsh7th/cmp-path",
-  "hrsh7th/cmp-nvim-lua",
-  "hrsh7th/cmp-nvim-lsp",
-  "onsails/lspkind-nvim",
   {
-    "saadparwaiz1/cmp_luasnip",
-    dependencies = { "L3MON4D3/LuaSnip" },
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-nvim-lua",
+      "hrsh7th/cmp-nvim-lsp",
+      "saadparwaiz1/cmp_luasnip",
+      {
+        "onsails/lspkind-nvim",
+        config = function()
+          require("lspkind").init({
+            symbol_map = {
+              Copilot = "",
+            },
+          })
+          vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#6CC644" })
+        end,
+      },
+      "roobert/tailwindcss-colorizer-cmp.nvim",
+    },
+    opts = function()
+      local cmp = require("cmp")
+      return {
+        completion = {
+          completeopt = "menu,menuone,noinsert",
+        },
+        mapping = {
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<c-y>"] = cmp.mapping(
+            cmp.mapping.confirm({
+              behavior = cmp.ConfirmBehavior.Insert,
+              select = true,
+            }),
+            { "i", "c" }
+          ),
+          ["<m-y>"] = cmp.mapping(
+            cmp.mapping.confirm({
+              behavior = cmp.ConfirmBehavior.Replace,
+              select = false,
+            }),
+            { "i", "c" }
+          ),
+          ["<c-space>"] = cmp.mapping({
+            i = cmp.mapping.complete(),
+            c = function(_)
+              if cmp.visible() then
+                if not cmp.confirm({ select = true }) then
+                  return
+                end
+              else
+                cmp.complete()
+              end
+            end,
+          }),
+          ["<tab>"] = cmp.config.disable,
+        },
+        snippet = {
+          expand = function(args)
+            require("luansip").lsp_expand(args.body)
+          end,
+        },
+        sources = cmp.config.sources({
+          { name = "nvim_lua" },
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "copilot" },
+        }, {
+          { name = "path" },
+          { name = "buffer", keyword_length = 5 },
+        }),
+        formatting = {
+          format = require("lspkind").cmp_format({
+            before = require("tailwindcss-colorizer-cmp").formatter,
+            with_text = true,
+            menu = {
+              buffer = "[buf]",
+              nvim_lsp = "[LSP]",
+              nvim_lua = "[api]",
+              path = "[path]",
+              luasnip = "[snip]",
+            },
+          }),
+        },
+      }
+    end,
   },
-  "roobert/tailwindcss-colorizer-cmp.nvim",
-
   {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
@@ -27,5 +107,8 @@ return {
       },
     },
   },
-  "zbirenbaum/copilot-cmp",
+  {
+    "zbirenbaum/copilot-cmp",
+    opts = {},
+  },
 }
