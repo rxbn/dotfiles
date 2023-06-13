424ec1a6e89b75a4219a4a24dc7f2665c53d7b93
Robin Scherrer <mail@robinscherrer.com>
2023-06-13 16:44:27 +0200

    refactor(nvim): catppuccin

--- config/nvim/lua/plugins/catppuccin.lua
+++ config/nvim/lua/plugins/catppuccin.lua
@@ -4,5 +4,22 @@ return {
     name = "catppuccin",
     lazy = false,
     priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "frappe",
+        integrations = {
+          harpoon = true,
+          lsp_saga = true,
+          mason = true,
+          treesitter = true,
+          treesitter_context = true,
+          dap = {
+            enabled = true,
+            enable_ui = true,
+          },
+        },
+      })
+      vim.cmd.colorscheme("catppuccin")
+    end,
   },
 }
