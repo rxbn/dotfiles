ebc505f52504c93d444c823f10fb445685aa6596
Robin Scherrer <mail@robinscherrer.com>
2023-06-22 18:54:15 +0200

    feat: add tmux-sshionizer

--- initial_setup.sh
+++ initial_setup.sh
@@ -141,6 +141,7 @@ ln -sf "${repo_path}/config/zsh/zshrc" ~/.config/zsh/.zshrc
 ln -sf "${repo_path}/config/yamllint/config" ~/.config/yamllint/config
 ln -sf "${repo_path}/bin/ansible-vault-pass" ~/bin/ansible-vault-pass
 ln -sf "${repo_path}/bin/tmux-sessionizer" ~/bin/tmux-sessionizer
+ln -sf "${repo_path}/bin/tmux-sshionizer" ~/bin/tmux-sshionizer
 ln -sf "${repo_path}/work/kubeconfigs-envrc" ~/work/kubeconfigs/.global-envrc
 ln -sf "${repo_path}/cargo/config.toml" ~/.cargo/config.toml
 
