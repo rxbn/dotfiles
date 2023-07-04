dbfc7fd7c02188a440c73120389981bd4410681e
Robin Scherrer <mail@robinscherrer.com>
2023-07-05 00:36:03 +0200

    fix(sketchybar): calendar props

--- config/sketchybar/items/calendar.sh
+++ config/sketchybar/items/calendar.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 calendar=(
-	icon=cal
 	icon.font="$FONT:Black:12.0"
-	icon.padding_right=0
-	label.width=70
-	label.align=right
 	padding_left=15
 	update_freq=1
 	script="$PLUGIN_DIR/calendar.sh"
