93d19cf51d07dcaeeb1d91e32beade2fec8ddebb
Robin Scherrer <mail@robinscherrer.com>
2023-07-18 16:34:35 +0200

    feat(sketchybar): use official meteoswiss icons

--- config/sketchybar/items/weather.sh
+++ config/sketchybar/items/weather.sh
@@ -2,12 +2,25 @@
 
 source "${HOME}/.config/zsh/personal_env"
 
-weather=(
+weather_icon=(
+	label.drawing=off
+	icon.drawing=off
+	padding_left=0
+	padding_right=-5
+	background.image.scale=0.0125
+	background.image.drawing=on
+	background.drawing=on
+	click_script="/usr/bin/open https://www.meteoswiss.admin.ch/local-forecasts/${CITY}/${ZIP_CODE}.html#forecast-tab=detail-view"
+)
+
+weather_temperature=(
 	icon.font="$FONT:Black:14.0"
 	update_freq=600
 	script="$PLUGIN_DIR/weather.sh"
 	click_script="/usr/bin/open https://www.meteoswiss.admin.ch/local-forecasts/${CITY}/${ZIP_CODE}.html#forecast-tab=detail-view"
 )
 
-sketchybar --add item weather right \
-	--set weather "${weather[@]}"
+sketchybar --add item weather_temperature right \
+	--set weather_temperature "${weather_temperature[@]}" \
+	--add item weather_icon right \
+	--set weather_icon "${weather_icon[@]}"
--- config/sketchybar/plugins/weather.sh
+++ config/sketchybar/plugins/weather.sh
@@ -5,8 +5,8 @@ source "${HOME}/.config/zsh/personal_env"
 WEATHER=$(curl --max-time 2 --silent "https://app-prod-ws.meteoswiss-app.ch/v1/plzDetail?plz=${ZIP_CODE}00")
 TEMPERATURE=$(jq '.currentWeather.temperature' <<<"$WEATHER")
 ICON=$(jq '.currentWeather.iconV2' <<<"$WEATHER")
-BAR_ICON=$(${CONFIG_DIR}/plugins/weather_icon_map.sh "$ICON")
 
-sketchybar --set $NAME label="${TEMPERATURE} °C" \
-	--set $NAME \
-	icon="$BAR_ICON"
+sketchybar -m --set weather_temperature label="${TEMPERATURE} °C" \
+	--set weather_icon \
+	background.image="${HOME}/Nextcloud/Photos/meteoswiss-weather-icons/png/${ICON}.png" \
+	background.color=0x00000000
