79c31b8bfb214afca089ff21a1b35b9e55e61937
Robin Scherrer <mail@robinscherrer.com>
2023-07-19 10:47:39 +0200

    fix(sketchybar): add weather icon padding

--- config/sketchybar/items/weather.sh
+++ config/sketchybar/items/weather.sh
@@ -4,6 +4,7 @@ source "${HOME}/.config/zsh/personal_env"
 
 weather=(
 	icon.font="$FONT:Black:18.0"
+	icon.padding_right=5
 	update_freq=600
 	script="$PLUGIN_DIR/weather.sh"
 	click_script="/usr/bin/open https://www.meteoswiss.admin.ch/local-forecasts/${CITY}/${ZIP_CODE}.html#forecast-tab=detail-view"
