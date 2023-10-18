37c90e054f3c1daaaf05d5165f48686b0d658e76
Robin Scherrer <mail@robinscherrer.com>
2023-10-18 18:09:30 +0200

    feat(sketchybar): use official meteoswiss icons

--- config/sketchybar/items/weather.sh
+++ config/sketchybar/items/weather.sh
@@ -2,7 +2,18 @@
 
 source "${HOME}/.config/zsh/personal_env"
 
-weather=(
+weather_icon=(
+	label.drawing=off
+	icon.drawing=off
+	padding_left=0
+	padding_right=-5
+	background.image.scale=0.0125
+	background.image.drawing=on
+	background.drawing=on
+	click_script="/usr/bin/open https://www.meteoswiss.admin.ch/local-forecasts/${CITY}/${ZIP_CODE}.html#forecast-tab=detail-view"
+)
+
+weather_temperature=(
 	icon.font="$FONT:Black:18.0"
 	icon.padding_right=5
 	update_freq=600
@@ -10,5 +21,7 @@ weather=(
 	click_script="/usr/bin/open https://www.meteoswiss.admin.ch/local-forecasts/${CITY}/${ZIP_CODE}.html#forecast-tab=detail-view"
 )
 
-sketchybar --add item weather right \
-	--set weather "${weather[@]}"
+sketchybar --add item weather_temperature right \
+	--set weather_temperature "${weather_temperature[@]}" \
+	--add item weather_icon right \
+	--set weather_icon "${weather_icon[@]}"
