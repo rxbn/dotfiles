b44289926af47417921ca3225231265ffa19490c
Robin Scherrer <mail@robinscherrer.com>
2023-11-27 10:48:32 +0100

    chore(nvim): cleanup linting config

--- config/nvim/lua/plugins/linting.lua
+++ config/nvim/lua/plugins/linting.lua
@@ -6,27 +6,19 @@ return {
     local lint = require("lint")
 
     lint.linters_by_ft = {
-      javascript = { "eslint_d" },
-      typescript = { "eslint_d" },
-      javascriptreact = { "eslint_d" },
-      typescriptreact = { "eslint_d" },
       yaml = { "yamllint" },
       ["yaml.ansible"] = { "ansible_lint" },
       go = { "golangcilint" },
       markdown = { "markdownlint" },
     }
 
-    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+    local augroup_lint = vim.api.nvim_create_augroup("custom-lsp-lint", { clear = true })
 
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-      group = lint_augroup,
+      group = augroup_lint,
       callback = function()
         lint.try_lint()
       end,
     })
-
-    vim.keymap.set("n", "<leader>l", function()
-      lint.try_lint()
-    end, { desc = "Trigger linting for current file" })
   end,
 }
