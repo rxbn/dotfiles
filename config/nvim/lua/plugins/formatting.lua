874738bce1be8c34192db200be78194ba3567647
Robin Scherrer <mail@robinscherrer.com>
2023-11-27 10:53:42 +0100

    chore(nvim): format lua

--- config/nvim/lua/plugins/formatting.lua
+++ config/nvim/lua/plugins/formatting.lua
@@ -1,7 +1,7 @@
 return {
   "stevearc/conform.nvim",
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
+  event = "BufWritePre",
+  cmd = "ConformInfo",
   config = function()
     local conform = require("conform")
 
