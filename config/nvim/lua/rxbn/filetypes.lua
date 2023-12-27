ad287f64a119625efee2dbae472c846c644cc595
Robin Scherrer <mail@robinscherrer.com>
2023-12-27 15:10:34 +0100

    feat(nvim): always map *.h to c filetype

--- config/nvim/lua/rxbn/filetypes.lua
+++ config/nvim/lua/rxbn/filetypes.lua
@@ -3,6 +3,11 @@ vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
   command = "set filetype=terraform",
 })
 
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = "*.h",
+  command = "set filetype=c",
+})
+
 if string.find(vim.fn.getcwd(), vim.fn.expand("~/personal/ansible")) then
   vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
     pattern = "*.yml",
