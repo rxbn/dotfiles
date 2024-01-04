1188734240b2a7bc8b78c2083c5dcb7d16e183e3
Robin Scherrer <mail@robinscherrer.com>
2024-01-04 21:51:44 +0100

    chore(nvim): improve dap listener setup

--- config/nvim/lua/plugins/debugging.lua
+++ config/nvim/lua/plugins/debugging.lua
@@ -24,14 +24,17 @@ return {
           local dap = require("dap")
           local dapui = require("dapui")
           dapui.setup(opts)
-          dap.listeners.after.event_initialized["dapui_config"] = function()
-            dapui.open({})
+          dap.listeners.before.attach.dapui_config = function()
+            dapui.open()
           end
-          dap.listeners.before.event_terminated["dapui_config"] = function()
-            dapui.close({})
+          dap.listeners.before.launch.dapui_config = function()
+            dapui.open()
           end
-          dap.listeners.before.event_exited["dapui_config"] = function()
-            dapui.close({})
+          dap.listeners.before.event_terminated.dapui_config = function()
+            dapui.close()
+          end
+          dap.listeners.before.event_exited.dapui_config = function()
+            dapui.close()
           end
         end,
       },
