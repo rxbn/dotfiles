c287fcc421645ac064cd65402b151188f1a5bc56
Robin Scherrer <mail@robinscherrer.com>
2024-01-05 17:21:31 +0100

    Revert "feat(nvim): improve go debugging"
    This reverts commit 5c91a3b6f2befdd878b4894493c1c920e5c914dd.

--- config/nvim/lua/plugins/debugging.lua
+++ config/nvim/lua/plugins/debugging.lua
@@ -43,68 +43,7 @@ return {
       },
       {
         "leoluz/nvim-dap-go",
-        config = function()
-          local pickers = require("telescope.pickers")
-          local finders = require("telescope.finders")
-          local conf = require("telescope.config").values
-          local actions = require("telescope.actions")
-          local action_state = require("telescope.actions.state")
-
-          require("dap-go").setup({
-            dap_configurations = {
-              {
-                name = "Find and launch main.go",
-                type = "go",
-                request = "launch",
-                cwd = "${workspaceFolder}",
-                program = function()
-                  return coroutine.create(function(coro)
-                    local opts = {}
-                    pickers
-                      .new(opts, {
-                        prompt_title = "Find main.go",
-                        finder = finders.new_oneshot_job({ "fd", "main.go" }, {}),
-                        sorter = conf.generic_sorter(opts),
-                        attach_mappings = function(buffer_number)
-                          actions.select_default:replace(function()
-                            actions.close(buffer_number)
-                            coroutine.resume(coro, action_state.get_selected_entry()[1])
-                          end)
-                          return true
-                        end,
-                      })
-                      :find()
-                  end)
-                end,
-              },
-              {
-                name = "Find and launch Go file",
-                type = "go",
-                request = "launch",
-                cwd = "${workspaceFolder}",
-                program = function()
-                  return coroutine.create(function(coro)
-                    local opts = {}
-                    pickers
-                      .new(opts, {
-                        prompt_title = "Find Go File",
-                        finder = finders.new_oneshot_job({ "fd", "--glob", "*.go" }, {}),
-                        sorter = conf.generic_sorter(opts),
-                        attach_mappings = function(buffer_number)
-                          actions.select_default:replace(function()
-                            actions.close(buffer_number)
-                            coroutine.resume(coro, action_state.get_selected_entry()[1])
-                          end)
-                          return true
-                        end,
-                      })
-                      :find()
-                  end)
-                end,
-              },
-            },
-          })
-        end,
+        opts = {},
       },
       {
         "mfussenegger/nvim-dap-python",
