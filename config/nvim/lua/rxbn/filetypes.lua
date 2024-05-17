ebc9c2c1f356dba403ce4c6e3a84e1a6081c0c3d
Robin Scherrer <mail@robinscherrer.com>
2024-05-17 08:38:16 +0200

    fix: ansible work config

--- config/nvim/lua/rxbn/filetypes.lua
+++ config/nvim/lua/rxbn/filetypes.lua
@@ -3,7 +3,10 @@ vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
   command = "set filetype=terraform",
 })
 
-if string.find(vim.fn.getcwd(), vim.fn.expand("~/personal/ansible")) then
+if
+  string.find(vim.fn.getcwd(), vim.fn.expand("~/personal/ansible"))
+  or string.find(vim.fn.getcwd(), vim.fn.expand("~/work/ansible"))
+then
   vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
     pattern = "*.yml",
     command = "set filetype=yaml.ansible",
