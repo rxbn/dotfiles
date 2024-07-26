f77798537f44c8f38c125a9850a312669f1e8d94
Robin Scherrer <mail@robinscherrer.com>
2024-07-26 21:39:12 +0200

    feat(sketchybar): improve weather plugin

--- config/sketchybar/plugins/weather.sh
+++ config/sketchybar/plugins/weather.sh
@@ -2,11 +2,17 @@
 
 source "${HOME}/.config/zsh/personal_env"
 
-WEATHER=$(curl --max-time 2 --silent "https://app-prod-ws.meteoswiss-app.ch/v1/plzDetail?plz=${ZIP_CODE}00")
-TEMPERATURE=$(jq '.currentWeather.temperature' <<<"$WEATHER")
-ICON=$(jq '.currentWeather.iconV2' <<<"$WEATHER")
+WEATHER=$(curl --max-time 2 --silent --retry 3 --retry-delay 1 --retry-connrefused "https://app-prod-ws.meteoswiss-app.ch/v1/plzDetail?plz=${ZIP_CODE}00")
+
+if [[ -n "$WEATHER" ]]; then
+  TEMPERATURE=$(jq '.currentWeather.temperature' <<<"$WEATHER")
+  ICON=$(jq '.currentWeather.iconV2' <<<"$WEATHER")
+else
+  TEMPERATURE="N/A"
+  ICON=""
+fi
 
 sketchybar -m --set weather_temperature label="${TEMPERATURE} °C" \
-	--set weather_icon \
-	background.image="${HOME}/Nextcloud/Photos/meteoswiss-weather-icons/png/${ICON}.png" \
-	background.color=0x00000000
+  --set weather_icon \
+  background.image="${HOME}/Nextcloud/Photos/meteoswiss-weather-icons/png/${ICON}.png" \
+  background.color=0x00000000
