4c938b0baecbc233dc906d2cf90c5b5beefd64fd
Robin Scherrer <mail@robinscherrer.com>
2025-05-01 14:38:31 +0200

    fix(nvim): use latest catppuccin

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -3,7 +3,6 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    tag = "stable",
     config = function()
       require("catppuccin").setup({
         flavour = "mocha",
